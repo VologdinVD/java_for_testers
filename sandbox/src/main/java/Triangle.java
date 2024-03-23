0d233ed9284e290154b131cbdef5991996e76505
Vologdin Vasilij <v.vologdin@cft.rul>
2024-03-23 18:22:23 +0700

    Рефакторинг: изменение class на record

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,14 +1,4 @@
-public class Triangle {
-
-    private int a;
-    private int b;
-    private int c;
-
-    public Triangle(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
+public record Triangle(int a, int b, int c) {
 
     public double perimeter() {
         return this.a + this.b + this.c;
