caeecba5a4edb96f6e6f506553862397587eb626
Vologdin Vasilij <v.vologdin@cft.rul>
2024-03-30 10:40:35 +0700

    Тренировка использования ветвлений и исключений

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,5 +1,13 @@
 public record Triangle(int a, int b, int c) {
 
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Стороны треугольника должны быть положительными");
+        } else if (a + b < c || a + c < b || c + b < a) {
+            throw new IllegalArgumentException("Сумма двух сторон треугольника должна быть больше третьей стороны");
+        }
+    }
+
     public double perimeter() {
         return this.a + this.b + this.c;
     }
--- sandbox/src/test/java/TriangleTest.java
+++ sandbox/src/test/java/TriangleTest.java
@@ -8,7 +8,7 @@ public class TriangleTest {
     @DisplayName("Вычисление периметра треугольника успешный сценарий")
     public void calcPerimeterOfTriangleSuccessTest() {
 
-        Triangle triangle = new Triangle(3, 4, 5);
+        Triangle triangle = new Triangle(3, 2, 5);
 
         double perimeterResult = triangle.perimeter();
         double expectedPerimeter = 12.0;
@@ -27,5 +27,28 @@ public class TriangleTest {
         double expectedSquare = 6.0;
 
         Assertions.assertEquals(expectedSquare, squareResult);
+    }
+
+    @Test
+    @DisplayName("Создание треугольника с отрицательными сторонами")
+    public void createTriangleWithNegativeSides() {
+
+        try {
+            new Triangle(-3, 4, 5);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
+
+    @Test
+    @DisplayName("Создание треугольника с суммой двух сторон меньшей, чем третья")
+    public void createTriangleWithSumSidesLessThird() {
+        try {
+            new Triangle(5, 5, 20);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
     }
 }
