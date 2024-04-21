3c118bdc8f2ad0609a0fbac0c8d73dd663130ff9
Vologdin Vasilij <v.vologdin@cft.rul>
2024-04-21 18:29:31 +0700

    Вынос описания тестового стенда в конфигурационный файл

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
 
     protected WebDriver driver;
@@ -16,7 +18,10 @@ public class ApplicationManager {
 
     private ContactHelper contacts;
 
-    public void init(String browser) {
+    private Properties properties;
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("chrome".equals(browser)) {
                 driver = new ChromeDriver();
@@ -26,9 +31,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1549, 822));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
